be221f713549db6de855fb463ea2c58ec71afc18
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-13 10:34:05 -0400

    method 1 assignment complete

---
+++ Methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment;
+
+internal class Methods
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello! My name is Computer. What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Ahh I see, so {name} is your name. Fantastic!");
+    }
+
+
+
+
+
+
+
+}   
+
+
+
